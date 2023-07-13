c7d0ce38ba1202bb7664a73ce459c92637a76b86
imadbourouche <ji_bourouche@esi.dz>
2023-07-13 21:31:05 +0100

    Init Module

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+	'name': "Client Spec",
+
+	'summary': """Gestion Clients Grossiste""",
+
+	'description': """
+	Gestion client grossiste module pour :
+	- Gerer client et commandes
+	- Gerer assurances
+	""",
+
+	'author': "Imad Bourouche",
+	'website': "http://www.imadcompany.com",
+	'license': 'LGPL-3',
+	'category': 'Test',
+	'version': '0.2',
+	'depends': [],
+	'data': [],
+	'demo': [],
+}
